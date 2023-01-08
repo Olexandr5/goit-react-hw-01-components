b6622ee022197bf083f42f140ca0653ce9db6db5
Olexandr Sherbakov <107261823+Olexandr5@users.noreply.github.com>
2023-01-08 13:34:21 +0200

    0.5
    0.5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,8 @@ import { Statistics } from 'components/Statistics/Statistics.jsx';
 import data from 'components/Statistics/data.json';
 import { FriendList } from 'components/FriendList/FriendList.jsx';
 import friends from 'components/FriendList/friends.json';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory.jsx';
+import transactions from 'components/TransactionHistory/transactions.json';
 
 export const App = () => {
   return (
@@ -16,10 +18,13 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
